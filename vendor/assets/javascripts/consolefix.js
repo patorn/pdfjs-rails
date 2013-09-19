7a04bacd87c9a221d14794e0a2373ae2904af23e
Patorn Utenpattanun <patorn.u@gmail.com>
2013-09-19 16:57:59 +0700

    Use PDFJS.version 0.7.381

--- vendor/assets/javascripts/consolefix.js
+++
@@ -1,9 +0,0 @@
-if(console && console.warn) {
-  // good
-} else if(console && console.log) {
-  console.warn = console.log;
-} else {
-  console = console || {};
-  console.warn = function() {};
-  console.log = function() {};
-}
--- vendor/assets/javascripts/pdfjs-rails.js
+++ vendor/assets/javascripts/pdfjs-rails.js
@@ -4,8 +4,8 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require consolefix.js
 //= require compatibility.js
 //= require l10n.js
 //= require pdf.js
-//= require_tree ./viewer
+//= require pdf.worker.js
+//= require viewer.js
